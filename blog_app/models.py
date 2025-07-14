8d35b5bd52c935c214b93380364fe88324b71f68
parashac <parashacharya139@gmail.com>
2025-07-14 17:25:32 +0545

    templates

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
-# Create your models here.
 class Post(models.Model):
-    title= models.CharField(max_length=255)
-    content = models.TextField()
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    published_at = models.DateTimeField(null=True, blank=True)
+    title=models.CharField(max_length=255)
+    content=models.TextField()
+    author=models.ForeignKey("auth.User",on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    published_at=models.DateTimeField(null=True,blank=True)
+def __str__(self):
+    return self.title
 
 
-    def __str__(self):
-        return self.title
 
 
 #1-1 relationships
