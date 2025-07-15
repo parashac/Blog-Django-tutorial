f40442f2d7933281ac9f1badb95faa1cf61643ac
parashac <parashacharya139@gmail.com>
2025-07-15 23:10:54 +0545

    Login,Logout and draft icon added

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,5 @@ from blog_app import views
 urlpatterns = [
     path("", views.post_list, name="post-list"),
     path("post-detail/<int:pk>/",views.post_detail, name="post-detail"),
+    path("draft-list/",views.draft_list, name="draft-list"),
 ]
