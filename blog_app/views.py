afcb75f2e87b719960bbb056c7704ee7d6703392
parashac <parashacharya139@gmail.com>
2025-07-16 18:07:41 +0545

    Login and logout added

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog_app.models import Post
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_at__isnull=False)
@@ -7,6 +8,12 @@ def post_list(request):
 def post_detail(request, pk):
     post =Post.objects.get(pk=pk)
     return render(request, "post_detail.html",{"post": post})
+@login_required
 def draft_list(request):
     posts = Post.objects.filter(published_at__isnull=True)
     return render(request, "draft_list.html", {"posts": posts})
+@login_required
+def draft_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    return render(request, "draft_detail.html", {"post": post})
+
